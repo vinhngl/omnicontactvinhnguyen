893a9403a7c53f64181efc926dd3cd1afb3d19de
Vinh Nguyen Le <vinh.nguyenlexuan@asnet.com.vn>
2014-09-27 22:30:06 +0700

    remove code unneccessary

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Omnicontacts::Application.routes.draw do
-  match "/contacts/:importer/callback" => "main#contact_hotmail", :via => [:get]
+  match "/contacts/:importer/callback" => "main#contact", :via => [:get]
 
   # Google
-  match "/oauth2callback" => "main#contact", :via => [:get]
+  # match "/oauth2callback" => "main#contact", :via => [:get]
 
   # Yahoo
-  match "/callback" => "main#contact_yahoo", :via => [:get]
+  # match "/callback" => "main#contact_yahoo", :via => [:get]
 
   # Yahoo
   # match "/contact_hotmail" => "main#contact_hotmail", :via => [:get]
